9f13b36a02604e7824f95a40be190ded396ebfc0
Jeff Wilkey <jeffrotull92@gmail.com>
2018-04-16 19:11:15 -0700

    Add POST action to blog-posts

--- server.js
+++ server.js
@@ -19,6 +19,20 @@ app.get('/blog-posts', (req, res) => {
   res.json(BlogPosts.get());
 });
 
+app.post('/blog-posts', jsonParser, (req, res) => {
+  // ensure `title` `content` and `author` are in request body
+  const requiredFields = ['title', 'content', 'author']
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const post = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate)
+  res.status(201).json(post);
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
